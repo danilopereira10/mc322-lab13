f60cf503296507c49d65e8351d2635c00c8b3cbc
danilo.pereira <danilopereira_b2@hotmail.com.br>
2020-06-21 15:03:50 -0300

    feat: default constructors for blueJewel and Tree

--- src/com/mc322/jewel_collector/rechargable/Tree.java
+++ src/com/mc322/jewel_collector/rechargable/Tree.java
@@ -1,7 +1,11 @@
 package com.mc322.jewel_collector.rechargable;
 
+import com.mc322.jewel_collector.environment.Point;
 import com.mc322.jewel_collector.obstacle.Obstacle;
+import com.mc322.jewel_collector.obstacle.ObstacleType;
 
 public class Tree extends Obstacle implements Rechargable {
-
+	public Tree(Point position) {
+		super(position, ObstacleType.TREE);
+	}
 }
